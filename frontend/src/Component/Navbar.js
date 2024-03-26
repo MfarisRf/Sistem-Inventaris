3f4a9f26feb5c77e214afc4f1e75bdd8706766e6
MUHAMAD FARIS RAFI <farisrafi2002@student.uns.ac.id>
2024-03-26 15:20:34 +0700

    90

--- frontend/src/Component/Navbar.js
+++ frontend/src/Component/Navbar.js
@@ -1,25 +1,78 @@
-import { Button, Navbar } from 'flowbite-react';
-import Logo from "./Assets/logo_header_light.png"
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import {AcmeLogo} from "./AcmeLogo";
+export default function Navbarlp() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-function Navbarlp() {
-  
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
   return (
-    <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg">
-    <div className="max-w-5xl mx-auto px-4">
-      <div className="flex items-center justify-between h-16">
-        <span className="text-2xl text-gray-900 font-semibold">
-        <img src={Logo} alt="logo" className="h-10 w-40" />
-        </span>
-        <div className="flex space-x-4 text-gray-900">
-          <a href="#">Dashboard</a>
-          <a href="#">About</a>
-          <a href="#">Projects</a>
-          <a href="#">Contact</a>
-        </div>
-      </div>
-    </div>
-  </nav>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
-
-export default Navbarlp
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}", 
-            "node_modules/flowbite-react/lib/esm/**/*.js"],
+            "node_modules/flowbite-react/lib/esm/**/*.js",
+            "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",],
+            
   theme: {
     extend: {},
   },
+  darkMode: "class",
+
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    nextui()
   ],
 }
